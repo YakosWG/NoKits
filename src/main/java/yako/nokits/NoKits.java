10309c37a9fe2646ae399e80a194c7e4676c8ee7
Yako <yakoswg@gmail.com>
2021-12-06 12:28:12 +0100

    Update Version

--- src/main/java/yako/nokits/NoKits.java
+++ src/main/java/yako/nokits/NoKits.java
@@ -17,7 +17,7 @@ public class NoKits {
 
     public static final String MODID = "nokits";
     public static final String NAME = "No Kits";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     public static Logger logger;
 
